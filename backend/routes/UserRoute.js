8172316bfb9c5c606465cd99fcae4854b322fc69
Dafs <rfkdafa03@gmail.com>
2025-07-02 16:05:48 +0700

    ubah userRoute

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -6,14 +6,13 @@ import {
   updateUser,
   deleteUser,
 } from "../controllers/Users.js";
-import { adminOnly, verifyUser } from "../middleware/AuthUser.js";
 
 const router = express.Router();
 
-router.get("/users", verifyUser, getUsers);
-router.get("/users/:id", verifyUser, getUserById);
+router.get("/users", getUsers);
+router.get("/users/:id", getUserById);
 router.post("/users", createUser);
-router.patch("/users/:id", verifyUser, adminOnly, updateUser);
-router.delete("/users/:id", adminOnly, deleteUser);
+router.patch("/users/:id", updateUser);
+router.delete("/users/:id", deleteUser);
 
 export default router;
